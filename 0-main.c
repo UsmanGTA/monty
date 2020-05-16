dd5b21cf91e819d5919e02c8d045e51e32ddfdcf
Usman Abdul Jabbar <46794411+UsmanGTA@users.noreply.github.com>
2020-05-16 00:47:25 -0500

    Revert "Stable Push 1.1"

--- 0-main.c
+++ 0-main.c
@@ -7,7 +7,7 @@
 #define OPEN_F "Error: Can't open file %s\n"
 #define BADCMD_F "L%d: unknown instruction %s\n"
 
-global_data univ = {NULL, NULL, NULL};
+baggage bag = {NULL, NULL, NULL};
 
 /**
  * main - gets lines from bytecode file
@@ -21,16 +21,14 @@ int main(int argc, char **argv)
 	size_t bufSize = 0;
 	ssize_t index, flag = 0, bytes;
 	stack_t *head = NULL;
-	unsigned int line_num = 0;
+	unsigned int line_count = 0;
 	FILE *fp;
-	instruction_t func[] = {
-		{"pall", pall_s}, {"pop", pop_s}, {"add", add_s}, {"nop", nop_s},
-		{"pint", pint_s}, {"swap", swap_s}, {"sub", sub_s}, {"mul", mul_s},
-		{"div", div_s}, {"mod", mod_s}, {"pchar", pchar_s}, {"pstr", pstr_s},
-		{"push", push_s},
+	instruction_t opcodes[] = {
+		{"pall", pall_s}, {"pop", pop_s}, {"add", add_s},
+		{"nop", nop_s}, {"pint", pint_s},
+		{"swap", swap_s}, {"sub", sub_s}, {"mul", mul_s}, {"div", div_s},
+		{"mod", mod_s}, {"pchar", pchar_s}, {"pstr", pstr_s}, {"push", push_s}
 	};
-	
-	/* FILE OPERATIONS */
 	if (argc != 2)
 		dprintf(2, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 	if (access(PROGRAM, R_OK) == -1)
@@ -38,23 +36,24 @@ int main(int argc, char **argv)
 	fp = fopen(PROGRAM, "r");
 	if (fp == NULL)
 		dprintf(2, "Error: malloc failed\n"), exit(EXIT_FAILURE);
-	univ.fp = fp; /* If all the above tests passed, assign fp */
-
-	/* EXECUTION LOOP */
+	bag.fp = fp;
 	while ((bytes = getline(&buf, &bufSize, fp)) != -1)
 	{
-		buf[bytes - 1] = '\0', univ.buf = buf, line_num++;
+		line_count++, buf[bytes - 1] = '\0', bag.buf = buf;
 		args[0] = strtok(buf, " ");
 		if (args[0] == NULL || args[0][0] == '#')
 			continue;
-		args[1] = strtok(NULL, " "), univ.data = DATA;
+		args[1] = strtok(NULL, " "), bag.data = DATA;
 		for (index = 0; index < 13; index++)
-			if (strcmp(CMD, func[index].opcode) == 0)
-				func[index].f(&head, line_num), flag = 1;
+			if (strcmp(CMD, opcodes[index].opcode) == 0)
+				opcodes[index].f(&head, line_count), flag = 1;
 		if (flag == 0) /* Check if flag flipped, if not, cmd not found */
-			dprintf(2, BADCMD_F, line_num, CMD), free_stack(head), rip();
+		{
+			dprintf(2, BADCMD_F, line_count, CMD),
+			free_stack(head), free(buf), fclose(fp), exit(EXIT_FAILURE);
+		}
 		flag = 0; /* Reset flag to check next loop */
 	}
-	free_stack(head), rip();
+	free(buf), free_stack(head), fclose(fp);
 	return (0);
 }
--- 2-logic-continued.c
+++ 2-logic-continued.c
@@ -1,104 +1,88 @@
 #include "monty.h"
 
-#define MALLOC_F "Error: malloc failed\n"
-#define EMPTYSTACK "L%d: usage: push integer\n"
-#define MISSINGDATA "L%d: usage: push integer\n"
 #define CANTADD "L%d: can't add, stack too short\n"
-#define PINT_F "L%d: can't pint, stack empty\n"
-#define POP_F "L%d: can't pop an empty stack\n"
+#define EMPTYSTACK "L%d: usage: push integer\n"
+#define MALLOC_F "Error: malloc failed\n"
+#define MISSINGDATA "L%d: usage: push integer\n"
+#define FREEALL (free(bag.buf), fclose(bag.fp), exit(EXIT_FAILURE))
+#define FREESTACK (free_stack(stack_t *stack))
 
 /**
- * pop_s - pops from top of stack
- * @stack: double pointer to stack
- * @line_number: line count
+ * push_s - pushes an integer to the stack
+ * with the help of malloc and a doubly
+ * linked list
+ * @stack: doubly linked list
+ * @line_number: linenumber
  * Return: Always void
  */
 
-void pop_s(stack_t **stack, unsigned int line_number)
+void push_s(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *temp = *stack; /* Stores the address of head */
+	stack_t *da_new_king = malloc(sizeof(stack_t)); /* Our new node */
+	int n, i;
 
-	if (stack == NULL || *stack == NULL)
-		dprintf(2, POP_F, line_number), rip();
-
-	/* Move head to the next node while old head's in temp */
-	*stack = temp->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-
-	free(temp); /* Free the old head */
-}
-
-/**
- * add_s - adds the very last two nodes added to the stack
- * then frees both of them
- * @stack: doubly linked list
- * @line_number: line number
- * Return: Void Always
- */
-void add_s(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp = *stack; /* *secondlastnode, *lastnode; */
-	int secondlast, last;
-	int total;
-	int nodecount;
-
-	for (temp = *stack, nodecount = 0; temp != NULL;
-	     temp = temp->next, nodecount++)
-		;
-
-	/* Check if we have two nodes to add */
-	if (stack == NULL || *stack == NULL || nodecount < 2)
+	if (da_new_king == NULL)
+		dprintf(2, MALLOC_F), exit(EXIT_FAILURE);
+	if (bag.data == NULL)
 	{
-		dprintf(2, CANTADD, line_number);
-		free_stack(*stack), fclose(univ.fp);
-		free(univ.buf), exit(EXIT_FAILURE);
+		dprintf(2, MISSINGDATA, line_number);
+		free(da_new_king), free_stack(*stack), FREEALL;
 	}
-	/* Pull the values from the last two nodes, then add */
-	secondlast = (*stack)->next->n, last = (*stack)->n;
-	total = secondlast + last;
-	temp = (*stack)->next;
-	      pop_s(stack, line_number);
-	      (*stack)->n = total;
-}
-
-/**
- * swap_s - swaps data between the first two nodes
- * @stack: doubly linked list
- * @line_number: contains the line number
- * Return: Always Void
- */
-void swap_s(stack_t **stack, unsigned int line_number)
-{
-	int firstnodedata, secondnodedata;
-	stack_t *temp = *stack;
-
-	/* While two nodes exist, swap the data */
-	if (temp && temp->next)
-	{
-		/* Pull the values from the two nodes */
-		firstnodedata = temp->n;
-		secondnodedata = temp->next->n;
-		/* Then swap them */
-		temp->n = secondnodedata, temp->next->n = firstnodedata;
-	}
-	else
-	{
-		dprintf(2, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-}
-
-/**
- * pint_s - prints the value of the head node
- * @stack: doubly linked list
- * @line_number: line number
- * Return: Always void
- */
-void pint_s(stack_t **stack, unsigned int line_number)
-{
+	else if (strcmp(bag.data, "0") == 0)
+		n = 0;
+	for (i = 0; bag.data[i] != '\0'; i++)
+		if ((bag.data[i] >= '0' && bag.data[i] <= '9') || bag.data[i] == '-')
+			continue;
+		else
+		{
+			dprintf(2, MISSINGDATA, line_number),
+			free(da_new_king), free_stack(*stack), FREEALL;
+		}
+	n = atoi(bag.data);
+	/* Populating data into the new node */
+	da_new_king->n = n;
+	da_new_king->prev = NULL;
 	if (*stack == NULL)
-		dprintf(2, PINT_F, line_number), exit(EXIT_FAILURE);
+	{
+		da_new_king->next = NULL; /* Terminate the linked list */
+		*stack = da_new_king; /* Copy new node to *head */
+	}
 	else
-		dprintf(1, "%d\n", (*stack)->n);
+	{
+		da_new_king->next = temp; /* Else our new node should point to head */
+		temp->prev = da_new_king; /*Point the next node->prev to the new head node */
+		*stack = da_new_king; /* Our new node is now the head */
+	}
+}
+
+/**
+ * pall_s - prints everything loaded onto the stack
+ * @stack: doubly linked list
+ * @line_number: for reference
+ * Return: Always void
+ */
+
+void pall_s(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp; /** Temp pointer to move forward */
+
+	(void)line_number; /* Voiding since var not in use */
+
+	if (stack != NULL || *stack != NULL)
+		for (temp = *stack; temp != NULL; temp = temp->next)
+			dprintf(1, "%d\n", temp->n);
+}
+
+/**
+ * nop_s - opcode that doesn't do anythin
+ * @stack: stack to be evaluated
+ * @line_number: line count
+ * Return: always void
+ */
+
+void nop_s(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
 }
