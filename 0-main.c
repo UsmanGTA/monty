1b23f9863002eb626136f12787fd80e7cc5d59df
Usman Abdul Jabbar <46794411+UsmanGTA@users.noreply.github.com>
2020-05-16 00:52:13 -0500

    Revert "Revert "Stable Push 1.1""

--- 0-main.c
+++ 0-main.c
@@ -7,7 +7,7 @@
 #define OPEN_F "Error: Can't open file %s\n"
 #define BADCMD_F "L%d: unknown instruction %s\n"
 
-baggage bag = {NULL, NULL, NULL};
+global_data univ = {NULL, NULL, NULL};
 
 /**
  * main - gets lines from bytecode file
@@ -21,14 +21,16 @@ int main(int argc, char **argv)
 	size_t bufSize = 0;
 	ssize_t index, flag = 0, bytes;
 	stack_t *head = NULL;
-	unsigned int line_count = 0;
+	unsigned int line_num = 0;
 	FILE *fp;
-	instruction_t opcodes[] = {
-		{"pall", pall_s}, {"pop", pop_s}, {"add", add_s},
-		{"nop", nop_s}, {"pint", pint_s},
-		{"swap", swap_s}, {"sub", sub_s}, {"mul", mul_s}, {"div", div_s},
-		{"mod", mod_s}, {"pchar", pchar_s}, {"pstr", pstr_s}, {"push", push_s}
+	instruction_t func[] = {
+		{"pall", pall_s}, {"pop", pop_s}, {"add", add_s}, {"nop", nop_s},
+		{"pint", pint_s}, {"swap", swap_s}, {"sub", sub_s}, {"mul", mul_s},
+		{"div", div_s}, {"mod", mod_s}, {"pchar", pchar_s}, {"pstr", pstr_s},
+		{"push", push_s},
 	};
+	
+	/* FILE OPERATIONS */
 	if (argc != 2)
 		dprintf(2, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 	if (access(PROGRAM, R_OK) == -1)
@@ -36,24 +38,23 @@ int main(int argc, char **argv)
 	fp = fopen(PROGRAM, "r");
 	if (fp == NULL)
 		dprintf(2, "Error: malloc failed\n"), exit(EXIT_FAILURE);
-	bag.fp = fp;
+	univ.fp = fp; /* If all the above tests passed, assign fp */
+
+	/* EXECUTION LOOP */
 	while ((bytes = getline(&buf, &bufSize, fp)) != -1)
 	{
-		line_count++, buf[bytes - 1] = '\0', bag.buf = buf;
+		buf[bytes - 1] = '\0', univ.buf = buf, line_num++;
 		args[0] = strtok(buf, " ");
 		if (args[0] == NULL || args[0][0] == '#')
 			continue;
-		args[1] = strtok(NULL, " "), bag.data = DATA;
+		args[1] = strtok(NULL, " "), univ.data = DATA;
 		for (index = 0; index < 13; index++)
-			if (strcmp(CMD, opcodes[index].opcode) == 0)
-				opcodes[index].f(&head, line_count), flag = 1;
+			if (strcmp(CMD, func[index].opcode) == 0)
+				func[index].f(&head, line_num), flag = 1;
 		if (flag == 0) /* Check if flag flipped, if not, cmd not found */
-		{
-			dprintf(2, BADCMD_F, line_count, CMD),
-			free_stack(head), free(buf), fclose(fp), exit(EXIT_FAILURE);
-		}
+			dprintf(2, BADCMD_F, line_num, CMD), free_stack(head), rip();
 		flag = 0; /* Reset flag to check next loop */
 	}
-	free(buf), free_stack(head), fclose(fp);
+	free_stack(head), rip();
 	return (0);
 }
